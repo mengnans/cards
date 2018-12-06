aaeb66e262fd8a31b4cd10e876b2b6f4ad9115cb
mengnans <mengnans@student.unimelb.edu.au>
2018-12-07 09:38:30 +1100

    Update api token and back-end url.

--- src/js/constants/constant.js
+++ src/js/constants/constant.js
@@ -2,13 +2,13 @@
  * the url of the back-end
  * @type {string}
  */
-export const BACKEND_URL = "https://atr-test-dh1.aiam-dh.com/atr-gateway/ticket-management/api/v1/tickets";
+export const BACKEND_URL = "https://atr-dev-dh.aiam-dh.com/atr-gateway/ticket-management/api/v1/tickets";
 
 /**
  * api token
  * @type {string}
  */
-export const API_TOKEN = "eyJhbGciOiJSUzUxMiIsInppcCI6IkRFRiJ9.eNqUkt1qg0AQhd9lrqU0aiB41a2OsmhcWddKKUWMbotQV_GHFsR37yZ3LSWmt2e-MzMczgLjfAIH3oZOTVLVkxwnMKCc60aqSurJpzxpoRpkOckanN3etg7mYW9au3vbgF4ObTOOTadGcF4WUGV7NnEkXnEkSULjQLv7odPg1EgNLetq_OSIK-gTFgKPSUQEXuNdbRBY5IyHfsTyzdWCuiGKImUZdzHdxGMmqE9dIiiLr8GC0yBAfvsf1MNYUPG8CfoUI--mKC74P4PYxMKY5RF6AT6SdPs6z6K_Qn014H3o5v7cCCCCF1mKHLQqv_pLgWzbsizT0kJbNh-_2vdQVpVU0zzIu6prYf0GAAD__w.hA1UxpMEszy7oIY-DJWeOk9iuaShq0GMx1V2LROfmTX6Goi1VYwYvnNLkPke_19-Ianku9e9pwF7hJNgkS8szOl4w_12x3UbDRXzPw7cRMmbDhlyRDXM-q8l7YGtVEBgn7t4yDjgdKnK5tB7ayDvb1tbE6IatvVW0Z42XSAchfQ";
+export const API_TOKEN = "eyJhbGciOiJSUzUxMiIsInppcCI6IkRFRiJ9.eNqU0t9LhEAQB_D_ZZ4lPM67Op_adJVFz5V1TSJCPN0LKX-gKwXi_956UQ8Raa_f-cyw7MwI_XACE85dU0tRF1L0EjTIhqIUdS5U5U2cVJB3IpOiAHOzM4zNdrc_6PvtjQat6Kqy78um7sF8HKHOqrmJYWSnRxSGJHBVd9s1CspSKDROk_btLAU5ThPKPMenyV_0MpITy8M8jWjMLBwt8oBy4hALcUKDRUxsHHDCHxahQ7CvnoKPoa8ev8i9gCY-tl18h6JljSxO7vGq6ZwR18Vs_e_N0KfIXgX_sY9FxmL_t209afDcNUM7nw4gztI4wgxUKsv8RUj3q6YS8d5-3t61oRsHXQVVVr7-ONzbs7jKmwqmDwAAAP__.KSaEHsfucpA5IZ-RpDq6H0OZ4O8GHUZ_BX5PEXNJrABx3ro6InEXkxKPTdmRRYbpuvQzK7px7EAcRz0xvcXe2T0A1OjEAAU4F-h4TmKAT34cPBCKapIEZfnvytN9rTnEru3Hw2jpHGNeqwFF1Gt75iViwePZRLIpeHFNUC-8UVE";
 
 /**
  * data per page
