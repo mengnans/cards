03bac235ddf972ae42a76a6603bae1f36a13cda1
mengnans <mengnans@student.unimelb.edu.au>
2018-12-07 09:25:39 +1100

    Update test cases

--- test/react-components/DrawerHeader.test.js
+++ test/react-components/DrawerHeader.test.js
@@ -4,7 +4,7 @@ import DrawerHeader from "../../src/js/presentational/DrawerHeader";
 
 
 describe('<DrawerHeader/>', () => {
-  it('<DrawerHeader> contains the nubmer props', () => {
+  it('<DrawerHeader> contains the number props', () => {
     const number = "INC0000040";
     const wrapper = shallow(<DrawerHeader number={number} onClickExit={()=>{}}
     />
