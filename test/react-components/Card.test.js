76b318418120e11c5737ac4b06142acfb3cfaec4
mengnans <mengnans@student.unimelb.edu.au>
2018-12-06 22:59:25 +1100

    Update tests

--- test/react-components/Card.test.js
+++ test/react-components/Card.test.js
@@ -2,19 +2,30 @@ import {shallow} from 'enzyme';
 import Card from "../../src/js/presentational/Card";
 import React from 'react';
 import mockData from "../../src/js/data-fetch/mock-data";
-import Typography from '@material-ui/core/Typography';
 
 
 describe('<Card/>', () => {
   it('while loading, contains .Card-loading-div class', () => {
     const wrapper = shallow(<Card isLoading={true} loadingInfo={"Loading"} progressBarColor={"primary"} noData={false}
-                                  coreData={mockData[0].coreData} onClick={() => (console.log("clicked"))}/>);
+    />);
     expect(wrapper.find('.Card-loading-div').exists()).toBeTruthy();
   });
 
   it('while is not loading, doesnt contains .Card-loading-div class', () => {
-    const wrapper = shallow(<Card isLoading={true} loadingInfo={"Loading"} progressBarColor={"primary"} noData={false}
-                                  coreData={mockData[0].coreData} onClick={() => (console.log("clicked"))}/>);
+    const wrapper = shallow(<Card isLoading={false} loadingInfo={"Loading"} progressBarColor={"primary"} noData={false}
+                                  coreData={mockData[0].coreData}/>);
     expect(wrapper.find('.Card-loading-div').exists()).toBeFalsy();
+  });
+
+  it('while is not loading, and no coreData', () => {
+    const wrapper = shallow(<Card isLoading={false} loadingInfo={"Loading"} progressBarColor={"primary"} noData={false}
+                                  coreData={mockData[0].coreData}/>);
+    expect(wrapper.find('.Card-loading-div').exists()).toBeFalsy();
+  });
+
+  it('while no data, let user know theres no data in the backend for this card item', () => {
+    const wrapper = shallow(<Card isLoading={false} loadingInfo={"Loading"} progressBarColor={"primary"} noData={true}
+    />);
+    expect(wrapper.find('.Card-loading-info').exists()).toBeTruthy();
   });
 });
